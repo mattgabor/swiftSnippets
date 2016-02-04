b716652b2f1b22c01b84582427a68ede1be9bb14
Matt Gabor <mattgabor@gmail.com>
2016-02-03 22:26:58 -0800

    added comments and fixed warnings

--- playgrounds/OptionalsExplanation.playground/Contents.swift
+++ playgrounds/OptionalsExplanation.playground/Contents.swift
@@ -7,6 +7,9 @@ var str = "Hello, playground"
 var someNum: Int?
 var someString: String?
 
+//MARK: if let
+
+// In this example, someNum has not been assigned, so it's value is initially nil.
 if let otherNum = someNum {
     print("success")
 } else {
@@ -15,26 +18,33 @@ if let otherNum = someNum {
 
 someNum = 0
 
+// Since someNum was assigned to 0 in line 17, it now contains a value, and will succeed on the if let check
 if let otherNum = someNum {
     print("success")
 } else {
-    
     print("nil")
 }
 
 
+// Similarly, this guard statement will fail because someString has not been assigned, and initially contains a nil value.
+
+// Note: with guard statements, the else block must exit the current scope, hence the return statements
+
 func guardFail() {
-    guard let otherString = someString else {
+    guard let _ = someString else {
         print("failure")
         return
     }
     print("success")
 }
 
+//MARK: guard
+
+// When someString gets assigned to "Hello World", the guard statement succeeds.
 func guardSucceed() {
     someString = "Hello World"
     
-    guard let otherString = someString else {
+    guard let _ = someString else {
         print("failure")
         return
     }
